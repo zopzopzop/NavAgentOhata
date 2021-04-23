08613e7d16616865c37fae20034669a101b8733d
Felipe Nascimento <35768605+zopzopzop@users.noreply.github.com>
2021-04-23 20:34:25 -0300

    Add projeto

---
+++ Assets/AgentManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AgentManager : MonoBehaviour
+{
+    GameObject[] agents;
+
+    void Start()
+    {
+        //encontra meus agents
+       
+        agents = GameObject.FindGameObjectsWithTag("ai");
+    }
+
+
+    void Update()
+    { 
+        //manda o agente para posição em que o mouse foi clicado.
+        if (Input.GetMouseButtonDown(0))
+        {
+            RaycastHit hit;
+
+            if (Physics.Raycast(Camera.main.ScreenPointToRay(Input.mousePosition), out hit, 100))
+            {
+                foreach (GameObject a in agents)
+                a.GetComponent<AiControl>().agent.SetDestination(hit.point);
+            }
+
+        }
+    }
+}
---
+++ Assets/AiControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+
+public class AiControl : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    void Start()
+    {
+        agent = this.GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
